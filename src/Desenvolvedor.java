a4bead5a2544ed00fd9abac797e91bcdb06c3ecb
Gustavo Colios <gustavocolioss@gmail.com>
2025-09-02 14:17:18 -0300

    a

--- src/Desenvolvedor.java
+++ src/Desenvolvedor.java
@@ -13,7 +13,6 @@ public Desenvolvedor(String nome, double salario, String linguagem) {
 
 //Método específico de um Desenvolvedor
     public void programar() {
-        System.out.println("O dev " + this.nome + " está programando em " + this.linguagem + ".");
 
     }
 
--- src/Empresa.java
+++ src/Empresa.java
@@ -8,11 +8,13 @@ public class Empresa {
 
         System.out.println("Dados do Gerente");
         System.out.println("Nome: " + gerente.getNome()); //Get nome e This são fixos, get nome serve para puxar o nome
+        System.out.println("Salário: " + gerente.getSalario());
         gerente.trabalhar(); //Método herdado de Funcionario
         gerente.aprovarVerba(); //Método específico de Gerente
 
         System.out.println("\nDados do Desenvolvedor");
         System.out.println("Nome: " + dev.getNome()); // Método herdado de Funcionário
+        System.out.println("Salário: " +dev.getSalario());
         dev.trabalhar(); //Método herdado de Funcionário
         dev.programar(); //Método específico de Desenvolver
     }
